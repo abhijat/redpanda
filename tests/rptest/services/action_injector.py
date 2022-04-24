d1b6fa9746538e7f3ed56d95b844c207edacd7e1
Abhijat Malviya <abhijat@redpanda.com>
2022-04-24 21:55:06 +0530

    tests: allows cluster to suppress process crashes
    Adds an option to @cluster to allow for missing redpanda processes,
    as the new chaos test can cause processes to die which is expected.
    
    Also changes some logging from warning to info, and allows override
    of config when using helper functions.
    
    the random action injected by the context is now applied before
    sleep, so that at least one action runs before sleep finishes.
    
    Also refactor the tests so that the base class is used for SI
    e2e tests, and uses Event instead of boolean to stop thread.
    
    when nodes are killed during tests, validations should not run
    on killed nodes, this commit syncs redpanda.started to remove
    killed nodes.
    
    also, the default replication factor is changed to num brokers,
    so that if the group leader is killed the system can still recover.

--- tests/rptest/services/action_injector.py
+++ tests/rptest/services/action_injector.py
@@ -1,7 +1,7 @@
 import dataclasses
 import random
 import time
-from threading import Thread
+from threading import Thread, Event
 from typing import Optional, List
 
 from ducktape.cluster.cluster import ClusterNode
@@ -21,11 +21,13 @@ class ActionConfig:
     max_affected_nodes: Optional[int] = None
 
     def random_time_in_range(self) -> float:
-        return random.uniform(self.min_time_between_actions_sec, self.max_time_between_actions_sec)
+        return random.uniform(self.min_time_between_actions_sec,
+                              self.max_time_between_actions_sec)
 
 
 class RandomNodeOp:
-    def __init__(self, redpanda: RedpandaService, config: ActionConfig, admin: Admin):
+    def __init__(self, redpanda: RedpandaService, config: ActionConfig,
+                 admin: Admin):
         self.admin = admin
         self.config = config
         self.redpanda = redpanda
@@ -37,7 +39,12 @@ class RandomNodeOp:
     def target_node(self) -> ClusterNode:
         available = set(self.redpanda.nodes) - self.nodes_affected
         if available:
-            return random.choice(list(available))
+            selected = random.choice(list(available))
+            names = {n.account.hostname for n in available}
+            self.redpanda.logger.info(
+                f'selected {selected.account.hostname} of {names} for operation'
+            )
+            return selected
 
     def action(self):
         raise NotImplementedError
@@ -54,26 +61,26 @@ class RandomNodeDecommission(RandomNodeOp):
     def action(self):
         node = self.target_node()
         if node:
-            broker_id = next((i for i, n in enumerate(self.redpanda.nodes) if n == node))
+            broker_id = next(
+                (i for i, n in enumerate(self.redpanda.nodes) if n == node))
             self.redpanda.stop_node(node)
             self.admin.decommission_broker(id=broker_id)
-            wait_until(
-                lambda: broker_id not in {b['node_id'] for b in self.admin.get_brokers()},
-                timeout_sec=120,
-                backoff_sec=2,
-                err_msg=f'Failed to decommission broker id {broker_id}'
-            )
+            wait_until(lambda: broker_id not in
+                       {b['node_id']
+                        for b in self.admin.get_brokers()},
+                       timeout_sec=120,
+                       backoff_sec=2,
+                       err_msg=f'Failed to decommission broker id {broker_id}')
             self.nodes_affected.add(node)
 
 
 class RandomLeadershipTransfer(RandomNodeOp):
-
     def __init__(
-            self,
-            redpanda: RedpandaService,
-            config: ActionConfig,
-            admin: Admin,
-            topics: List[TopicSpec],
+        self,
+        redpanda: RedpandaService,
+        config: ActionConfig,
+        admin: Admin,
+        topics: List[TopicSpec],
     ):
         super().__init__(redpanda, config, admin)
         self.topics = topics
@@ -84,14 +91,16 @@ class RandomLeadershipTransfer(RandomNodeOp):
     def action(self):
         for topic in self.topics:
             for partition in range(topic.partition_count):
-                old_leader = self.admin.get_partition_leader(namespace='kafka', topic=topic, partition=partition)
+                old_leader = self.admin.get_partition_leader(
+                    namespace='kafka', topic=topic, partition=partition)
                 self.admin.transfer_leadership_to(namespace='kafka',
                                                   topic=topic,
                                                   partition=partition,
                                                   target=None)
 
                 def leader_is_changed():
-                    new_leader = self.admin.get_partition_leader(namespace='kafka', topic=topic, partition=partition)
+                    new_leader = self.admin.get_partition_leader(
+                        namespace='kafka', topic=topic, partition=partition)
                     return new_leader != -1 and new_leader != old_leader
 
                 wait_until(leader_is_changed,
@@ -101,7 +110,8 @@ class RandomLeadershipTransfer(RandomNodeOp):
 
 
 class RandomNodeProcessFailure(RandomNodeOp):
-    def __init__(self, redpanda: RedpandaService, config: ActionConfig, admin: Admin):
+    def __init__(self, redpanda: RedpandaService, config: ActionConfig,
+                 admin: Admin):
         super(RandomNodeProcessFailure, self).__init__(redpanda, config, admin)
         self.failure_injector = FailureInjector(self.redpanda)
 
@@ -111,43 +121,55 @@ class RandomNodeProcessFailure(RandomNodeOp):
     def action(self):
         node = self.target_node()
         if node:
-            self.failure_injector.inject_failure(FailureSpec(FailureSpec.FAILURE_KILL, node))
+            self.redpanda.logger.info(
+                f'executing action on {node.account.hostname}')
+            self.failure_injector.inject_failure(
+                FailureSpec(FailureSpec.FAILURE_KILL, node))
             self.nodes_affected.add(node)
+
+            # Update started_nodes so validations are run on the correct
+            # set of nodes later.
+            try:
+                self.redpanda.started_nodes().remove(node)
+            except ValueError:
+                self.redpanda.logger.warn(
+                    f'failed to remove {node} from rp node list')
+        else:
+            self.redpanda.logger.warn(f'no usable node')
 
 
 class ActionInjectorThread(Thread):
     def __init__(
-            self,
-            config: ActionConfig,
-            redpanda: RedpandaService,
-            random_op: RandomNodeOp,
-            *args,
-            **kwargs,
+        self,
+        config: ActionConfig,
+        redpanda: RedpandaService,
+        random_op: RandomNodeOp,
+        *args,
+        **kwargs,
     ):
         self.random_op = random_op
         self.redpanda = redpanda
         self.config = config
-        self._stop_requested = False
+        self._stop_requested = Event()
         super().__init__(*args, **kwargs)
 
     def run(self):
-        wait_until(
-            lambda: self.redpanda.healthy(),
-            timeout_sec=self.config.cluster_start_lead_time_sec,
-            backoff_sec=2,
-            err_msg=f'Cluster not ready to begin actions'
-        )
+        wait_until(lambda: self.redpanda.healthy(),
+                   timeout_sec=self.config.cluster_start_lead_time_sec,
+                   backoff_sec=2,
+                   err_msg=f'Cluster not ready to begin actions')
 
-        while not self._stop_requested:
-            time.sleep(self.config.random_time_in_range())
+        while not self._stop_requested.is_set():
             self.random_op()
+            time.sleep(self.config.random_time_in_range())
 
     def stop(self):
-        self._stop_requested = True
+        self._stop_requested.set()
 
 
 class ActionCtx:
-    def __init__(self, config: ActionConfig, redpanda: RedpandaService, random_op: RandomNodeOp):
+    def __init__(self, config: ActionConfig, redpanda: RedpandaService,
+                 random_op: RandomNodeOp):
         self.redpanda = redpanda
         self.config = config
         if config.max_affected_nodes is None:
@@ -155,29 +177,48 @@ class ActionCtx:
         self.thread = ActionInjectorThread(config, redpanda, random_op)
 
     def __enter__(self):
+        self.redpanda.logger.info(f'entering random failure ctx')
         self.thread.start()
 
     def __exit__(self, *args, **kwargs):
+        self.redpanda.logger.info(f'leaving random failure ctx')
         self.thread.stop()
+        self.thread.join()
 
 
-def create_context_with_defaults(redpanda: RedpandaService, op_type, *args, **kwargs):
+def create_context_with_defaults(redpanda: RedpandaService,
+                                 op_type,
+                                 config: ActionConfig = None,
+                                 *args,
+                                 **kwargs):
     admin = Admin(redpanda)
-    config = ActionConfig(
-        cluster_start_lead_time_sec=120,
+    config = config or ActionConfig(
+        cluster_start_lead_time_sec=20,
         min_time_between_actions_sec=10,
         max_time_between_actions_sec=30,
     )
-    return ActionCtx(config, redpanda, op_type(redpanda, config, admin, *args, **kwargs))
+    return ActionCtx(config, redpanda,
+                     op_type(redpanda, config, admin, *args, **kwargs))
 
 
-def random_process_kills(redpanda: RedpandaService):
-    return create_context_with_defaults(redpanda, RandomNodeProcessFailure)
+def random_process_kills(redpanda: RedpandaService,
+                         config: ActionConfig = None):
+    return create_context_with_defaults(redpanda,
+                                        RandomNodeProcessFailure,
+                                        config=config)
 
 
-def random_decommissions(redpanda: RedpandaService):
-    return create_context_with_defaults(redpanda, RandomNodeDecommission)
+def random_decommissions(redpanda: RedpandaService,
+                         config: ActionConfig = None):
+    return create_context_with_defaults(redpanda,
+                                        RandomNodeDecommission,
+                                        config=config)
 
 
-def random_leadership_transfers(redpanda: RedpandaService, topics):
-    return create_context_with_defaults(redpanda, RandomLeadershipTransfer, topics)
+def random_leadership_transfers(redpanda: RedpandaService,
+                                topics,
+                                config: ActionConfig = None):
+    return create_context_with_defaults(redpanda,
+                                        RandomLeadershipTransfer,
+                                        topics,
+                                        config=config)
